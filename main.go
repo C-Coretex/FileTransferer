62d015bb4f31904aa44251cbdd905bc86c6c3fcc
C-Coretex <valerash2@gmail.com>
2020-07-06 20:12:35 +0300

    The program works fine now, it's able to transfer files, but it can't transfer files between different computers

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import(
 	 "net"
 	 "strconv"
 	 "strings"
+	 "io"
 )
 
 //FileExists gets the full path to the file
@@ -18,12 +19,120 @@ func FileExists(filename string) bool {
     return !info.IsDir()
 }
 
+func fillString(retunString string, toLength int) string {
+	for {
+		lengtString := len(retunString)
+		if lengtString < toLength {
+			retunString = retunString + ":"
+			continue
+		}
+		break
+	}
+	return retunString
+}
+
+func sendFileToClient(connection net.Conn, path string) {
+	Output.Println("A client has connected!")
+	defer connection.Close()
+	file, err := os.Open(path)
+	if err != nil {
+		Output.Println(err)
+		return
+	}
+	fileInfo, err := file.Stat()
+	if err != nil {
+		Output.Println(err)
+		return
+	}
+	fileSize := fillString(strconv.FormatInt(fileInfo.Size(), 10), 10)
+	fileName := fillString(fileInfo.Name(), 64)
+	Output.Println("Sending filename and filesize!")
+	connection.Write([]byte(fileSize))
+	connection.Write([]byte(fileName))
+	sendBuffer := make([]byte, BUFFERSIZE)
+	Output.Println("Start sending file!")
+	for {
+		_, err = file.Read(sendBuffer)
+		if err == io.EOF {
+			Output.Println("Error:", err)
+			break
+		}
+		connection.Write(sendBuffer)
+		
+		if err == io.EOF{
+			break
+		}
+	}
+	Output.Println("File has been sent, closing connection!")
+	return
+}
+
+//StartServer function is the function to start the server
+func StartServer(path string, IP string){
+	server, err := net.Listen("tcp4", ":8001")
+	if err != nil {
+		Output.Println("Error listetning: ", err)
+		os.Exit(1)
+	}
+	defer server.Close()
+	Output.Println("Server started! Waiting for connections...")
+	for {
+		connection, err := server.Accept()
+		if err != nil {
+			Output.Println("Error: ", err)
+			os.Exit(1)
+		}
+		Output.Println("Client connected")
+		go sendFileToClient(connection, path)
+	}
+}
+
+//StartClient is the function to start the client
+func StartClient(IP string){
+	connection, err := net.Dial("tcp", IP)
+	if err != nil {
+		panic(err)
+	}
+	defer connection.Close()
+	Output.Println("Connected to server, start receiving the file name and file size")
+	bufferFileName := make([]byte, 64)
+	bufferFileSize := make([]byte, 10)
+	
+	connection.Read(bufferFileSize)
+	fileSize, _ := strconv.ParseInt(strings.Trim(string(bufferFileSize), ":"), 10, 64)
+	
+	connection.Read(bufferFileName)
+	fileName := strings.Trim(string(bufferFileName), ":")
+	
+	newFile, err := os.Create(fileName)
+	
+	if err != nil {
+		panic(err)
+	}
+	defer newFile.Close()
+	var receivedBytes int64
+	
+	for {
+		if (fileSize - receivedBytes) < BUFFERSIZE {
+			io.CopyN(newFile, connection, (fileSize - receivedBytes))
+			connection.Read(make([]byte, (receivedBytes+BUFFERSIZE)-fileSize))
+			break
+		}
+		io.CopyN(newFile, connection, BUFFERSIZE)
+		receivedBytes += BUFFERSIZE
+	}
+	Output.Println("Received file completely!")
+}
+
 const (
 	//Send file to the server (client)
 	Send = iota + 1
 	//Get file from the client (server)
 	Get
 )
+
+//BUFFERSIZE is the size of the buffer that will be sent to the client
+const BUFFERSIZE = 1024
 
 func main() {
 	conn, err := net.Dial("udp", "8.8.8.8:80")
@@ -37,8 +146,8 @@ func main() {
 	
 	Repeat:
 	
-	Output.Println("{1} - Send a file (Client)")
-	Output.Println("{2} - Get a file (Server)")
+	Output.Println("{1} - Send a file (Server)")
+	Output.Println("{2} - Get a file (Client)")
 	reader := Input.NewReader(os.Stdin)
 	option, _ := reader.ReadString('\n')
 	option = strings.TrimRight(option, "\r\n")
@@ -57,19 +166,23 @@ func main() {
 			goto FileDoesNotExist
 		}
 		
-		Output.Println("\nIP of the server")
-		server, _ := reader.ReadString('\n')
-		server = strings.TrimRight(server, "\r\n")
-		
-		Output.Println(path, " ", server)
+		StartServer(path, localAddr.String());
 		
 	} else if(option == strconv.Itoa(Get)){
-		Output.Println("\nYou want to get file")
+		Output.Println("\nIP of the server")
+		serverIP, _ := reader.ReadString('\n')
+		serverIP = strings.TrimRight(serverIP, "\r\n")
+		
+		StartClient(serverIP);
 		
 	} else{
 		goto Repeat
 		
 	}
 	 
+	
+	
 	Output.Println();
+	Output.Scanf("h")
+	_, _ = reader.ReadString('\n')
 }
